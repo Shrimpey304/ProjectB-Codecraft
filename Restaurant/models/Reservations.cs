6652196a4d823118c58750de8d90030d0befd3f5
cachopreto <144028585+cachopreto@users.noreply.github.com>
2023-10-29 13:56:59 +0100

    reservation backend update and restaurant format

---
+++ Restaurant/models/Reservations.cs
@@ -0,0 +1,18 @@
+namespace Restaurant;
+
+public class Reservations
+{
+    public DateOnly ReservationDate {get;set;}
+    public List<Table>? TablesList {get;set;}
+
+    public Reservations(DateOnly reservation)
+    {
+        ReservationDate = reservation;
+        //Tables = new();
+    }
+
+    public override string ToString()
+    {
+        return $"Date:{ReservationDate} Tables:{TablesList}";
+    }
+}
