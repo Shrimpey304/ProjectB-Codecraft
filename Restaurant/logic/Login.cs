61439ef5eeeb56de4c446579259d21aa5f387793
Shrimpey304 <kimo.edelbroek@gmail.com>
2023-11-13 15:08:07 +0100

    added login functionalitys

--- Restaurant/logic/Login.cs
+++ Restaurant/logic/Login.cs
@@ -5,6 +5,9 @@ namespace Restaurant;
 public class Login
 {
 
+    public static string LoggedinUser = "";
+    public static bool IsLoggedIn = false;
+
     private static string accountPath = @".\dataStorage\account.json";
     public static bool PassMatches(User Password){
 
@@ -38,6 +41,19 @@ public class Login
         string key  = mail;
         string val = Password;
 
-        
+        List<User> accounts = File.Exists(accountPath) 
+        ? JsonConvert.DeserializeObject<List<User>>(File.ReadAllText(accountPath) ?? string.Empty) ?? new List<User>()
+        : new List<User>(); 
+
+        Console.WriteLine(accounts);
+
+    }
+
+    public static bool getStatus(){
+        return IsLoggedIn;
+    }
+
+    public static string getUser(){
+        return LoggedinUser;
     }
 }
