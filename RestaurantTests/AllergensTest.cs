6985e3c891bfcb5c9e0d9c68b1cb034b2b37938d
cachopreto <144028585+cachopreto@users.noreply.github.com>
2023-11-28 21:21:13 +0100

    Update AllergensTest.cs

--- RestaurantTests/AllergensTest.cs
+++ RestaurantTests/AllergensTest.cs
@@ -2,14 +2,14 @@ using System.Dynamic;
 using System.Runtime.InteropServices;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Restaurant;
-
+[TestClass]
 public class RemoveAllergensTest
 {
     [TestMethod]
     public void RemoveAllergens_AllergenRemoved_ReturnsString()
     {
         // Arrange
-        var allergen = new Allergens(); // Note: You might need to instantiate your Allergens class
+        //Allergen allergen = new Allergen(); // Note: You might need to instantiate your Allergens class
 
         // Act
         string result = FoodManager.RemoveAllergens("Gluten");
