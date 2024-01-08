2ec976bef2cf7a0408fefc11168b435fdc070e85
cachopreto <144028585+cachopreto@users.noreply.github.com>
2024-01-08 13:10:15 +0100

    small bugs by menucard fixed

--- Restaurant/userside/RegisterProcess.cs
+++ Restaurant/userside/RegisterProcess.cs
@@ -69,46 +69,46 @@ public class RegisterProcess{
     }
 
 
-    // public void RegisterProcessView(){
+    public void RegisterProcessView(){
 
-    //     Console.WriteLine("welcome to registration (alpha version)");    
-    //     Console.WriteLine("please enter your Email");
-    //     string ?GivenEmail = "";
-    //     while(Registration.CheckEmailRegEx(GivenEmail!) == false){
-    //         Console.Clear();
-    //         Console.WriteLine("invalid email format");
-    //         string mail = Console.ReadLine()!;
-    //         GivenEmail += mail + "";
-    //     }
+        Console.WriteLine("welcome to registration (alpha version)");    
+        Console.WriteLine("please enter your Email");
+        string ?GivenEmail = "";
+        while(Registration.CheckEmailRegEx(GivenEmail!) == false){
+            Console.Clear();
+            Console.WriteLine("invalid email format");
+            string mail = Console.ReadLine()!;
+            GivenEmail += mail + "";
+        }
 
-    //     Console.WriteLine("please enter your password"); 
-    //     Console.WriteLine("Capital, lower, number");
-    //     string ?GivenPW = "";
-    //     while(Registration.CheckPasswordFormat(GivenPW!) == false){
-    //         string pass = Registration.HashPassword();
-    //         GivenPW += pass + "";
-    //     }
-    //     Console.WriteLine(GivenPW);
+        Console.WriteLine("please enter your password"); 
+        Console.WriteLine("Capital, lower, number");
+        string ?GivenPW = "";
+        while(Registration.CheckPasswordFormat(GivenPW!) == false){
+            string pass = Registration.HashPassword();
+            GivenPW += pass + "";
+        }
+        Console.WriteLine(GivenPW);
         
 
-    //     Console.Write("please retype the password\n");
-    //     string ?RetypePW = Registration.HashPassword();
-    //     if(Registration.CheckPasswordSimilar(GivenPW!, RetypePW!) == true){
-    //         Console.WriteLine("registratie succesvol");
-    //     }else{
-    //         Console.WriteLine("failed");
-    //     }
+        Console.Write("please retype the password\n");
+        string ?RetypePW = Registration.HashPassword();
+        if(Registration.CheckPasswordSimilar(GivenPW!, RetypePW!) == true){
+            Console.WriteLine("registratie succesvol");
+        }else{
+            Console.WriteLine("failed");
+        }
 
-    //     Console.WriteLine("please write your phone number");
-    //     string PhoneNR = Console.ReadLine()!;
-    //     Registration.CheckPhoneNumberFormat(PhoneNR);
+        Console.WriteLine("please write your phone number");
+        string PhoneNR = Console.ReadLine()!;
+        Registration.CheckPhoneNumberFormat(PhoneNR);
 
-    //     User user = REG.CreateAccount(GivenEmail, GivenPW, PhoneNR!, false);
-    //     ingelogdmenu.user = user;
-    //     // ingelogdmenu.logOut.Add(REG);
-    //     JsonUtil.UpdateSingleObject(user, filePath);
-    //     Login.IsLoggedIn = true;
-    //     Login.LoggedinUser = user.Email;
-    //     ingelogdmenu.DisplayIngelogdMenu();
-    // }
+        User user = REG.CreateAccount(GivenEmail, GivenPW, PhoneNR!, false);
+        ingelogdmenu.user = user;
+        // ingelogdmenu.logOut.Add(REG);
+        JsonUtil.UpdateSingleObject(user, filePath);
+        Login.IsLoggedIn = true;
+        Login.LoggedinUser = user.Email;
+        ingelogdmenu.DisplayIngelogdMenu();
+    }
 }
